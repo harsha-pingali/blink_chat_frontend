cc1e66f35d58be796bd4fca1163c726e4b4c6f5b
harsha <saiharshapingali@gmail.com>
2024-02-02 11:51:07 +0530

    modified chat ui

--- src/Components/Chat/ScrollableChat.js
+++ src/Components/Chat/ScrollableChat.js
@@ -6,7 +6,7 @@ import {
   isSameSender,
   isSameSenderMargin,
   isSameUser,
-} from "../config/ChatLogics.js";
+} from "../config/ChatLogics";
 import { ChatState } from "../../context/ChatProvider";
 
 const ScrollableChat = ({ messages }) => {
@@ -16,7 +16,7 @@ const ScrollableChat = ({ messages }) => {
     <ScrollableFeed>
       {messages &&
         messages.map((m, i) => (
-          <div style={{ display: "flex" }} key={m._id}>
+          <div style={{ display: "flex", padding: "20px" }} key={m._id}>
             {(isSameSender(messages, m, i, user._id) ||
               isLastMessage(messages, i, user._id)) && (
               <Tooltip label={m.sender.name} placement="bottom-start" hasArrow>
@@ -35,11 +35,11 @@ const ScrollableChat = ({ messages }) => {
                 backgroundColor: `${
                   m.sender._id === user._id ? "#BEE3F8" : "#B9F5D0"
                 }`,
-                marginLeft: isSameSender(messages, m, i, user._id) ? 0 : 33,
+                marginLeft: isSameSenderMargin(messages, m, i, user._id),
                 marginTop: isSameUser(messages, m, i, user._id) ? 3 : 10,
                 borderRadius: "20px",
                 padding: "5px 15px",
-                maxWidth: "100%",
+                maxWidth: "75%",
               }}
             >
               {m.content}
